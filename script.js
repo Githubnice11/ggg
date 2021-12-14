4dbe3b66650201d89c39cb98e7682f0f815caa3a
Тимур <tt9647417@gmail.com>
2021-12-14 21:38:37 +0500

    Sdelano

---
+++ script.js
@@ -0,0 +1,92 @@
+// console.log(1);
+// console.log(4.5);
+// console.log("Моя первая строка");
+// console.log('Моя первая строка');
+// console.log(true);
+// console.log(false);
+
+
+
+// var countUsers = 5;
+// console.log(countUsers);
+ 
+// var number = 3;
+// console.log(5 + 6);
+// console.log(8 - 7);
+// console.log(8 - 10);
+// console.log(5 * 9);
+// console.log(9 / 3);
+// console.log(10 % 3); 
+
+
+// console.log("Меня зовут " + "Вася");
+// var myName = "Vasya";
+
+// var hisName = myName;
+// console.log(hisName);
+
+// alert("Вызвал модальное окно");
+// alert(hisName)
+// var myName= promt("Введите ваше имя")
+// console.log(myName);
+
+// var num = prompt ("Сколько вам лет")
+// var number = 55; 
+// console.log(+num);
+// console.log(number);
+// console.log("6 + 3 =" + (6 + 3));
+
+
+// var age; 
+// console.log(age);
+// var age;
+// console.log("dadasda");
+// age = 55 
+// console.log(age);
+
+// var name = null; 
+// console.log(name);
+
+
+// var age = 25;
+// console.log(age);
+// age = 26;
+// console.log(age);
+var name = prompt("Введите ваше имя");
+console.log(name);
+var num = prompt("Сколько вам лет");
+console.log(num);
+    
+
+var chislo1 = prompt ("Сколько будет 5/5")
+console.log("Ваш ответ " + chislo1  );
+var otvet = "Пример 1: 5/5 = 1" ;
+console.log(otvet);
+
+var chislo2 = prompt ("Сколько будет 5%5")
+console.log("Ваш ответ " + chislo2  );
+var otvet = "Пример 1: 5%5 = 0" ;
+console.log(otvet);
+
+
+
+var chislo = prompt ("Сколько будет 5*5")
+console.log("Ваш ответ " + chislo  );
+var otvet = "Пример 1: 5*5 = 25" ;
+console.log(otvet);
+
+var chislo3 = prompt ("Сколько будет 5-5")
+console.log("Ваш ответ " + chislo3  );
+var otvet = "Пример 1: 5-5 = 0" ;
+console.log(otvet);
+ 
+var chislo4 = prompt ("Сколько будет 5+5")
+console.log("Ваш ответ " + chislo4  );
+var otvet = "Пример 1: 5+5 = 10" ;
+console.log(otvet);
+
+
+// console.log("5*5  =" + (5*5));
+
+ 
+ 
